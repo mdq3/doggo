d30d3b1999e55fdafcb9d7b462630383a2f52aeb
mdq3 <1479991+mdq3@users.noreply.github.com>
2026-05-02 10:09:57 +0100

    Add grid to blocks coding area

--- doggo-blocks/src/App.tsx
+++ doggo-blocks/src/App.tsx
@@ -78,6 +78,7 @@ export function App() {
       toolbox: toolboxConfig,
       theme: doggoTheme,
       media: '/media/',
+      grid: { spacing: 24, length: 1, colour: '#c0c0c0', snap: true },
       scrollbars: true,
       zoom: { controls: true, wheel: true, startScale: 0.85 },
       sounds: false,
